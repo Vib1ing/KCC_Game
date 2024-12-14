8d76cb089a480f88721fcdc9644f53071bc303cf
lemonpeppereel <elijahpark01@gmail.com>
2024-12-14 10:08:20 -0500

    added some player movement

---
+++ Assets/Circle.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public float movSpeed = 10;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.linearVelocity = new Vector2(speedX, speedY);
+    }
+}
